cd50d5d12c41ab02b24218713ee8d96b58b0969f
joycethh <joycethh@outlook.com>
2022-10-12 12:48:09 -0500

    connect to MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 import "dotenv/config";
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
+//express app setup
 const app = express();
 
-const PORT = process.env.PORT;
+//middleware
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
+const PORT = process.env.PORT;
+const URL = process.env.MONGODB_URL;
+
+//for testing
 app.get("/", (req, res) => {
   res.send("APP IS RUNNING");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is runing on port ${PORT}`);
-});
+//DB connection
+mongoose
+  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is runing on port ${PORT} and DB is connected`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
