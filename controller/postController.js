35a6e0f3ae47bd0531a6d835fe39d5667686bbd8
joycethh <joycethh@outlook.com>
2022-10-27 11:32:26 -0500

    add like post function

--- controller/postController.js
+++ controller/postController.js
@@ -56,6 +56,21 @@ export const updatePost = async (req, res) => {
   res.json(updatedPost);
 };
 
+//like post
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const seletedPost = await postMessage.findById(id);
+  const updatedPost = await postMessage.findByIdAndUpdate(
+    id,
+    { likes: seletedPost.likes + 1 },
+    { new: true }
+  );
+  res.json(updatedPost);
+};
+
 //delete one
 export const deletePost = async (req, res) => {
   const { id } = req.params;
